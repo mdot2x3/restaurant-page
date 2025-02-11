41693f22bdee5ce2479c6c0c472eb7e81a36b230
mdot2x3 <mdot2x3@gmail.com>
2025-02-10 16:06:31 -0800

    style: nav bar updates
    round content section banner corners.
    
    add logo icon to nav bar and add additional div to
    ensure balanced spacing for nav buttons.
    
    set fixed-position background image so other
    content glides over it.
    
    change from buttons to li items.
    
    add li background image for selected li item and
    'on hover' pointer and text highlights.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import "./style.css";
 
 // alert("This works.");
 // console.log("This also works.");
+
+// apply background image to selected nav bar li button
+document.addEventListener("DOMContentLoaded", function() {
+    const listItems = document.querySelectorAll("nav ul li");
+
+    listItems.forEach(item => {
+        item.addEventListener("click", function() {
+            listItems.forEach(i => i.classList.remove("selected"));
+
+            this.classList.add("selected");
+        });
+    });
+});
